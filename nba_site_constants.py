5a04ce57d9cf3558e05c4bd3f50cc91060c9b7be
Ben Brady <bradybt@umich.edu>
2018-10-17 00:29:35 -0700

    finished loader

--- nba_site_constants.py
+++ nba_site_constants.py
@@ -32,4 +32,19 @@ TEAM_PARAMETERS = {
 	'VsDivision': '',
 }
 
-TEAM_RATINGS_FILE = 'team_ratings.txt'
+OFFENSIVE_TEAM_RATINGS_FILE = 'oteam_ratings.txt'
+DEFENSIVE_TEAM_RATINGS_FILE = 'dteam_ratings.txt'
+
+REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive',
+    'Host': 'stats.nba.com',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+TEAM_INDEX = 1
+OFFENSIVE_RATING_INDEX = 8
+DEFENSIVE_RATING_INDEX = 10
--- rating_loader.py
+++ rating_loader.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import requests
 import os
@@ -5,7 +6,7 @@ import os
 import nba_site_constants
 
 def ScrapeURL(base_url, parameters):
-    response = requests.get(baseURL, params=parameters, headers=user_agent_headers)
+    response = requests.get(base_url, params=parameters, headers=nba_site_constants.REQUEST_HEADERS)
     response.raise_for_status()
     headers = response.json()['resultSets'][0]['headers']
     stats = response.json()['resultSets'][0]['rowSet']
@@ -16,8 +17,24 @@ def ScrapeURL(base_url, parameters):
 
 
 
-def main():
- 	print 'hello beautiful'
+def LoadRatings():
+ 	data_frame = ScrapeURL(nba_site_constants.TEAM_STATS_URL, 
+ 						   nba_site_constants.TEAM_PARAMETERS)
+
+ 	# map of team name to respective team ratings
+ 	offensive_rating = dict()
+ 	defensive_rating = dict()
+
+ 	for index, row in data_frame.iterrows():
+ 		print row
+		offensive_rating[row[nba_site_constants.TEAM_INDEX]] = row[nba_site_constants.OFFENSIVE_RATING_INDEX]
+		defensive_rating[row[nba_site_constants.TEAM_INDEX]] = row[nba_site_constants.DEFENSIVE_RATING_INDEX]
+
+	with open(nba_site_constants.OFFENSIVE_TEAM_RATINGS_FILE, 'w') as offensive_file:
+		offensive_file.write(json.dumps(offensive_rating))
+
+	with open(nba_site_constants.DEFENSIVE_TEAM_RATINGS_FILE, 'w') as defensive_file:
+		defensive_file.write(json.dumps(defensive_rating))
 
 if __name__ == "__main__":
-    main()
+    LoadRatings()
