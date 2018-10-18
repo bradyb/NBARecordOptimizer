d4638f91bfe9ec94762182c5141aa584b83befee
Ben Brady <bradybt@umich.edu>
2018-10-17 23:30:40 -0700

    Adding for constant support

--- model_runner.py
+++ model_runner.py
@@ -1,6 +1,13 @@
+import itertools
 import json
 import nba_site_constants
 import rating_loader
+
+def _ProbAWinsVsB(a_ortg, a_drtg, b_ortg, b_drtg):
+	a_win_pct = a_ortg**16.5/(a_ortg**16.5 + a_drtg**16.5)
+	b_win_pct = b_ortg**16.5/(b_ortg**16.5 + b_drtg**16.5)
+	return a_win_pct * (1 - b_win_pct)/(a_win_pct * (1 - b_win_pct) + b_win_pct * (1 - a_win_pct))
+
 
 def _LoadJSONFile(file_name):
 	with open(file_name) as file:
@@ -13,6 +20,10 @@ def FindOptimalPlan():
 
 	max_score = 0
 	permutation = None
+
+	for team_index in itertools.permutations(range(0,30), 24):
+		pass
+
 
 def FormatPlan(plan):
 	pass
