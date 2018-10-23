34274c53e4a3fa7800cce5214acdb7f3b704a85d
Ben Brady <bradybt@umich.edu>
2018-10-22 22:49:19 -0700

    Had to change to a greedy approach

--- model_runner.py
+++ model_runner.py
@@ -9,6 +9,15 @@ def _ProbAWinsVsB(a_ortg, a_drtg, b_ortg, b_drtg):
 	b_win_pct = b_ortg**16.5/(b_ortg**16.5 + b_drtg**16.5)
 	return a_win_pct * (1 - b_win_pct)/(a_win_pct * (1 - b_win_pct) + b_win_pct * (1 - a_win_pct))
 
+def _GetCurrentScore():
+	total_wins = 0
+	total_losses = 0
+	for wins, losses in nba_site_constants.PAST_SCORES:
+		total_wins += wins
+		total_losses += losses
+	return total_wins, total_losses
+
+
 
 def _LoadJSONFile(file_name):
 	with open(file_name) as file:
@@ -16,7 +25,7 @@ def _LoadJSONFile(file_name):
 	return data
 
 def _ScoreForWeek(team_name, week, oratings, dratings):
-    opponents = []
+	opponents = []
 	# Get the teams that the team plays this week
 	# Count the number of back to backs?
 	expected_wins = 0
@@ -25,40 +34,46 @@ def _ScoreForWeek(team_name, week, oratings, dratings):
 	                                                 dratings[team_name],
 	                                                 oratings[opponent],
 	                                                 dratings[opponent])
-    return expected_wins
+	return expected_wins
 
-def _ComputePlanScore(permutation):
-	score = 0
-	for team_index, week in itertools.izip(permutation,
-										   range(0, 24-nba_site_constants.WEEKS_PLAYED)):
-		score = score + _ScoreForWeek(nba_site_constants.TEAMS[team_index], week)
-	return score
+def _GetNextteam(week, off_ratings, def_ratings, current_plan):
+	week_expectation = dict()
+	best_team = None
+	best_wins = 0
+	best_losses = 0
+	for team in nba_site_constants.TEAMS
+		wins, losses, team = _TeamForWeek(nba_site_constants.TEAMS[team], 
+										  week, 
+										  off_ratings, 
+										  def_ratings)
+	return best_wins, best_losses, best_team
 
-def FindOptimalPlan():
-	offensive_ratings = _LoadJSONFile(nba_site_constants.OFFENSIVE_TEAM_RATINGS_FILE)
-	defensive_ratings = _LoadJSONFile(nba_site_constants.DEFENSIVE_TEAM_RATINGS_FILE)
+def FindBestPlan():
+	offensive_ratings = _LoadJSONFile(nba_site_constants.
+										OFFENSIVE_TEAM_RATINGS_FILE)
+	defensive_ratings = _LoadJSONFile(nba_site_constants.
+										DEFENSIVE_TEAM_RATINGS_FILE)
 
-	max_score = 0
-	opt_permutation = None
+	total_wins, total_losses = _GetCurrentScore()
+	next_team = None
+	current_plan = dict()
 
-	for permutation in itertools.permutations(range(0,len(nba_site_constants.TEAMS) - nba_site_constants.WEEKS_PLAYED),
-													24 - nba_site_constants.WEEKS_PLAYED):
-		score = _ComputePlanScore(permutation)
-		if score > max_score:
-			max_score = score
-			opt_permutation = permutation
+	scores = _GetNextWeek(week, 
+						  offensive_ratings, 
+						  defensive_ratings, 
+						  current_plan)
 
-	return permutation
+	return current_plan
 
 
 
 def FormatPlan(plan):
-    print nba_site_constants.TEAMS[plan[0]]
+    return sorted( ((value, key) for key, value in plan.items()), reverse=True)
 
 
 def ComputeNextPick():
 	rating_loader.LoadRatings()
-	plan = FindOptimalPlan()
+	plan = FindBestPlan()
 	print FormatPlan(plan)
 
 if __name__ == "__main__":
--- nba_site_constants.py
+++ nba_site_constants.py
@@ -35,7 +35,7 @@ TEAM_PARAMETERS = {
 OFFENSIVE_TEAM_RATINGS_FILE = 'oteam_ratings.txt'
 DEFENSIVE_TEAM_RATINGS_FILE = 'dteam_ratings.txt'
 
-REQUEST_HEADERS = {
+REQUEST_HEADERS_OLD = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.8',
@@ -43,6 +43,20 @@ REQUEST_HEADERS = {
     'Host': 'stats.nba.com',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+}
+
+REQUEST_HEADERS = {
+	'Accept': 'application/json, text/plain, */*',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'en-US,en;q=0.9',
+	'Connection': 'keep-alive',
+	'Cookie': '_ga=GA1.2.1377756853.1539757763; ug=5b51910b01e4710a3c09a42b88002f3d; __gads=ID=99e90ba2f2ea239e:T=1539757764:S=ALNI_MYcZxwVPl4osHwbiUbDDXbM2o2QSg; _omappvp=rZqdlzsHNW0SRqZMt2AzaicL4xdNOCWDoIcQXsTBgEgYJNPDOINyv6rIFNHjegFn5bzsN5wKYIiQHCAgncCJVtZC9QxizgUu; check=true; AMCVS_248F210755B762187F000101%40AdobeOrg=1; AMCV_248F210755B762187F000101%40AdobeOrg=1687686476%7CMCIDTS%7C17828%7CMCMID%7C68712475597671435332818477153219687279%7CMCAAMLH-1540873734%7C9%7CMCAAMB-1540873734%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1540276134s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C3.0.0; _gid=GA1.2.1670959731.1540268935; s_cc=true; AMCVS_7FF852E2556756057F000101%40AdobeOrg=1; AMCV_7FF852E2556756057F000101%40AdobeOrg=1687686476%7CMCIDTS%7C17828%7CMCMID%7C68709941649562229942818129117327216912%7CMCAAMLH-1540873736%7C9%7CMCAAMB-1540873736%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1540276136s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C3.0.0; ugs=1; ak_bmsc=FB5C1ECFE8F2A2BBBD64921E6642371D409158EA761300009BA3CE5B87631921~pliptG3JddbHbgXO0V3uOHMXPs6ypun6AEaq8yHkPYW11KsYiXUtF7Ylh4mn22/5jIC0rYEM5e+Yax6RCbbahjqLoZfsWsCiPADZMJ2GyEPf2jyIrcmOXcu+aTrIIyQXUA+8ij7tjGYqKIV4uOijcKcr4n+3eUy8BkMiITBH5ifh30j9EDKPa7yZ4PzXeZeliACQuEsflY4lYqRTzZI4yekOYqBgdkJVtqgo66SM7DL4o=; s_sq=%5B%5BB%5D%5D; _gat=1; bm_sv=5A9F520871019C1B8849EBFF0AF13F38~/AxbKFekR5vR4V/S1ofZq/hcYxmkmv9GP0Rx9Wj1VmMwoBk3UUazC+LvNkjnQCnP0tuIgJIYfFYwENe/Kv0GeYr+WqQGRMootyuoO11+IeZaNGCxBq0vxrjhfabmqpcsnd4hyj1DD3Ee0lrg/QsGDQ==; mbox=PC#62c73d03aa7b417c929e547499f4c01e.28_3#1603002564|session#84992069b0ef4f6a8db8f56185c3cb40#1540270940',
+	'Host': 'stats.nba.com',
+	'Referer': 'https://stats.nba.com/teams/advanced/?sort=OFF_RATING&dir=-1&Season=2018-19&SeasonType=Regular%20Season',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+	'x-nba-stats-origin': 'stats',
+	'x-nba-stats-token': 'true',
+	'X-NewRelic-ID': 'VQECWF5UChAHUlNTBwgBVw=='
 }
 
 TEAM_INDEX = 1
@@ -80,6 +94,14 @@ TEAMS = [
 	"Memphis Grizzlies",
 	"New Orleans Pelicans",
 	"San Antonio Spurs"
+]
+
+PICKS = {
+	"Philadelphia 76ers": 0
+}
+
+PAST_SCORES = [
+	(2,1)
 ]
 
 TEAMNAME_MAP = {
