1ad53f4bed73b1a272f8282dc9ac53e137a27405
Ben Brady <bradybt@umich.edu>
2018-10-27 15:23:36 -0700

    getting close to making the first run

--- model_runner.py
+++ model_runner.py
@@ -6,17 +6,27 @@ import requests
 import nba_site_constants
 import rating_loader
 
+_DAYS_IN_WEEK = 7
 
 class PlanFinder:
 
+	def __init__(self):
+		with open('dates.txt', 'r') as dates_file:
+			day_list = dates_file.read().splitlines()
+		self.week_list = list()
+		for day in range(0, len(day_list), _DAYS_IN_WEEK):
+			self.week_list.append(day_list[day:day + _DAYS_IN_WEEK])
+
+		self.offensive_ratings = self._LoadJSONFile(nba_site_constants.
+										OFFENSIVE_TEAM_RATINGS_FILE)
+		self.defensive_ratings = self._LoadJSONFile(nba_site_constants.
+										DEFENSIVE_TEAM_RATINGS_FILE)
+		self.total_wins, self.total_losses = self._GetCurrentScore()
+
 	def _FormScheduleUrl(self, date):
-		return nba_site_constants.PROD_URL + str(date) + '/scoreboard.json'
+		return nba_site_constants.PROD_URL + date + '/scoreboard.json'
 
-        def _LoadDates():
-            with open('dates.txt', 'r') as dates_file:
-                return dates_file.readlines()
-
-        def _UpdateOpponents(self, teams_opponents, hteam_id, vteam_id):
+	def _UpdateOpponents(self, teams_opponents, hteam_id, vteam_id):
 		if hteam_id in teams_opponents:
 			teams_opponents[hteam_id].append(vteam_id)
 		else:
@@ -35,7 +45,12 @@ class PlanFinder:
 			self._UpdateOpponents(teams_opponents, game['hTeam']['teamId'], game['vTeam']['teamId'])
 		return teams_opponents
 
-	def _ProbAWinsVsB(self, a_ortg, a_drtg, b_ortg, b_drtg):
+	def _ProbAWinsVsB(self, team, opponent):
+		a_ortg = self.offensive_ratings[team]
+		a_drtg = self.defensive_ratings[team]
+		b_ortg = self.offensive_ratings[opponent]
+		b_drtg = self.defensive_ratings[opponent]
+
 		a_win_pct = a_ortg**16.5/(a_ortg**16.5 + a_drtg**16.5)
 		b_win_pct = b_ortg**16.5/(b_ortg**16.5 + b_drtg**16.5)
 		return a_win_pct * (1 - b_win_pct)/(a_win_pct * (1 - b_win_pct) + b_win_pct * (1 - a_win_pct))
@@ -53,40 +68,32 @@ class PlanFinder:
 			data = json.load(file)
 		return data
 
-	def _TeamScoreForWeek(self, team_name, opponents, oratings, dratings):
+	def _TeamScoreForWeek(self, team_name, week):
 		expected_wins = 0
 		for opponent in opponents:
-			expected_wins = expected_wins + _ProbAWinsVsB(oratings[team_name],
-	    		dratings[team_name],
-	    		oratings[opponent],
-	    		dratings[opponent])
+			expected_wins = expected_wins + _ProbAWinsVsB(team_name, opponent)
 		return expected_wins
 
-	def _GetWeekSchedule(self, week, weeks_list):
+	def _GetWeekSchedule(self, week):
 		games_dict = dict()
-		for day in weeks_list[week]:
+		for day in self.week_list[week]:
 			for team, opponents in self._GetSchedule(date):
 				games_dict[team].extend(opponents)
 
 
-	def _GetNextWeek(self, week, off_ratings, def_ratings, picks):
-		# Get Schedule for the week here
+	def _GetNextWeek(self, week, picks):
 		week_expectation = dict()
 		for team in nba_site_constants.TEAMS:
 			if team in picks:
 				continue
 			wins, losses, team = _TeamScoreForWeek(nba_site_constants.TEAMS[team], 
-											  week, 
-											  off_ratings, 
-											  def_ratings)
+											  week)
 			week_expectation[team] = {'wins': wins,
 									  'losses': losses}
 		return week_expectation
 
-	def _GetBestPlans(self, off_ratings, def_ratings, week,
-					 total_wins, total_losses, picks):
-		# Return Top 3 of Next Week
-		week_expectation = _GetNextWeek(week, off_ratings, def_ratings, picks)
+	def _GetBestPlans(self, week, picks):
+		week_expectation = _GetNextWeek(week, picks)
 		total_games = total_wins + total_losses
 		#compare = lambda (key, value): (value['wins'] + total_wins) / (total_games + value['wins'] + value['losses'])
 		#sorted_week = sorted(week_expectation.iteritems(), key=compare, reverse=True)
@@ -97,18 +104,10 @@ class PlanFinder:
 
 
 	def FindBestPlan(self):
-		offensive_ratings = _LoadJSONFile(nba_site_constants.
-										OFFENSIVE_TEAM_RATINGS_FILE)
-		defensive_ratings = _LoadJSONFile(nba_site_constants.
-										DEFENSIVE_TEAM_RATINGS_FILE)
+		plans = self._GetBestPlans(nba_site_constants.WEEKS_PLAYED, 
+								   nba_site_constants.PICKS.copy())
 
-		total_wins, total_losses = _GetCurrentScore()
-
-		plans = _GetBestPlans(offensive_ratings, defensive_ratings, 
-						nba_site_constants.WEEKS_PLAYED,
-						total_wins, total_losses, nba_site_constants.PICKS.copy())
-
-		return _BestPlan(plans)
+		return self._BestPlan(plans)
 
 
 
@@ -121,4 +120,4 @@ def ComputeNextPick():
 
 if __name__ == "__main__":
 	plan_finder = PlanFinder()
-	plan_finder._GetSchedule(20181028)
+	print(plan_finder._LoadDates())
